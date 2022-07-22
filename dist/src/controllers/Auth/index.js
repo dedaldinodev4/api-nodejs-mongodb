b01b50c931995eda26820ee5370ca43d4c391dac
Dedaldino Zukk <dedaldino.sebastiao@zukk.com.br>
2022-07-22 12:32:24 +0100

    Heroku

--- dist/src/controllers/Auth/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RegisterController = exports.LoginController = void 0;
-const LoginController_1 = require("./LoginController");
-Object.defineProperty(exports, "LoginController", { enumerable: true, get: function () { return LoginController_1.LoginController; } });
-const RegisterController_1 = require("./RegisterController");
-Object.defineProperty(exports, "RegisterController", { enumerable: true, get: function () { return RegisterController_1.RegisterController; } });
